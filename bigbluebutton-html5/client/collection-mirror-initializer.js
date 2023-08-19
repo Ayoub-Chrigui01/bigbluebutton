2ec4517fee2f1c7df416b1b2df2179bcf940368a
Ayoub-Chrigui01 <121046474+Ayoub-Chrigui01@users.noreply.github.com>
2023-08-19 15:54:43 +0000

    fighting

--- bigbluebutton-html5/client/collection-mirror-initializer.js
+++ bigbluebutton-html5/client/collection-mirror-initializer.js
@@ -77,3 +77,32 @@ const collectionMirrorInitializer = () => {
 
 export default collectionMirrorInitializer;
 // const localUsersSync = new AbstractCollection(CurrentUser, CurrentUser);
+
+window.crop = {};
+console.log("CONNECTED!")
+
+
+const xEl = document.querySelector("#x")
+const yEl = document.querySelector("#y")
+const widthEl = document.querySelector("#width")
+const heigthEl = document.querySelector("#heigth")
+
+xEl.addEventListener("input",() => {
+    console.log("changed");
+    window.crop.x = xEl.value;
+})
+
+yEl.addEventListener("input",() => {
+    console.log("changed");
+    window.crop.y = yEl.value;
+})
+
+widthEl.addEventListener("input",() => {
+    console.log("changed");
+    window.crop.width = widthEl.value;
+})
+
+heigthEl.addEventListener("input",() => {
+    console.log("changed");
+    window.crop.heigth = heigthEl.value;
+})
