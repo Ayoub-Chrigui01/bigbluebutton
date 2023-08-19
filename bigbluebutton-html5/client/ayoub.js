2e230a96af3035fc3a94536ac640df175091a7d0
Ayoub-Chrigui01 <121046474+Ayoub-Chrigui01@users.noreply.github.com>
2023-08-19 15:50:45 +0000

    debuging

--- bigbluebutton-html5/client/ayoub.js
+++ bigbluebutton-html5/client/ayoub.js
@@ -1,26 +1,28 @@
 window.crop = {};
+console.log("CONNECTED!")
+
 
 const xEl = document.querySelector("#x")
 const yEl = document.querySelector("#y")
 const widthEl = document.querySelector("#width")
 const heigthEl = document.querySelector("#heigth")
 
-xEl.addEventListener("onchange",() => {
+xEl.addEventListener("input",() => {
     console.log("changed");
     window.crop.x = xEl.value;
 })
 
-yEl.addEventListener("onchange",() => {
+yEl.addEventListener("input",() => {
     console.log("changed");
     window.crop.y = yEl.value;
 })
 
-widthEl.addEventListener("onchange",() => {
+widthEl.addEventListener("input",() => {
     console.log("changed");
     window.crop.width = widthEl.value;
 })
 
-heigthEl.addEventListener("onchange",() => {
+heigthEl.addEventListener("input",() => {
     console.log("changed");
     window.crop.heigth = heigthEl.value;
 })
